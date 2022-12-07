623c2f1c39c4b948923f2dca09451d05c516c099
granitnebiu <granitnebiu@gmail.com>
2022-12-07 23:03:40 +0100

    added default constants axios

---
+++ frontend/src/utils/axios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { config } from "src/utils/Constants";
+
+var url = config.url.API_URL;
+
+const request = axios.create({
+  baseURL: url,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+request.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status == 401) {
+      window.location.href = "/login";
+    }
+  }
+);
+
+export default request;
